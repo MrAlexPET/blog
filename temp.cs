b9a13d986f291c12ca3feefcd05a08ab12af9bd7
MrAlexPET <87830706+MrAlexPET@users.noreply.github.com>
2026-08-21 15:07:14 +0300

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,9 +1,1 @@
-dotnet remove package Microsoft.Extensions.Hosting.WindowsServices
-dotnet remove package Microsoft.Extensions.Hosting
-dotnet remove package Microsoft.Extensions.Logging.EventLog
-
-dotnet add package Microsoft.Extensions.Hosting --version 8.0.1
-
-dotnet add package Microsoft.Extensions.Hosting.WindowsServices --version 8.0.1
-
-dotnet add package Microsoft.Extensions.Logging.EventLog --version 8.0.1
+dotnet build LastAuthentication.Service\LastAuthentication.Service.csproj
