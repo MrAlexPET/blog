66bf173389ce597d688788e8a0a426bac82c8a3a
MrAlexPET <87830706+MrAlexPET@users.noreply.github.com>
2026-08-21 15:03:40 +0300

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -2,9 +2,7 @@ dotnet remove package Microsoft.Extensions.Hosting.WindowsServices
 dotnet remove package Microsoft.Extensions.Hosting
 dotnet remove package Microsoft.Extensions.Logging.EventLog
 
-dotnet remove package Microsoft.Extensions.Hosting.WindowsServices
-dotnet remove package Microsoft.Extensions.Hosting
-dotnet remove package Microsoft.Extensions.Logging.EventLog
+dotnet add package Microsoft.Extensions.Hosting --version 8.0.1
 
 dotnet add package Microsoft.Extensions.Hosting.WindowsServices --version 8.0.1
 
