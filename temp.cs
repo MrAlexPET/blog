f7fccffaffc5087005cc292156428f7878787dbc
MrAlexPET <87830706+MrAlexPET@users.noreply.github.com>
2026-08-21 10:44:23 +0300

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,86 +1,242 @@
-using Microsoft.Win32;
 using System;
-using System.Diagnostics;
-using System.IO;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Principal;
+using System.Xml.Linq;
 
 namespace LastAuthentication
 {
-    public class StartupManager
+    public enum LoginType
     {
-        private const string RunKey =
-            @"Software\Microsoft\Windows\CurrentVersion\Run";
+        Unknown,
+        Local,
+        Rdp
+    }
 
-        private const string AppName =
-            "LastAuthentication";
+    public class LoginInfo
+    {
+        public DateTime Time { get; set; }
 
-        public void EnableStartup()
+        public LoginType Type { get; set; }
+
+        public string LogonId { get; set; } = "";
+
+        public int WindowsSessionId { get; set; }
+    }
+
+    public class LoginDetector
+    {
+        public LoginInfo? FindCurrentLogin()
         {
-            try
+            WindowsIdentity identity =
+                WindowsIdentity.GetCurrent();
+
+            string sid =
+                identity.User?.Value ?? "";
+
+            if (string.IsNullOrEmpty(sid))
+                return null;
+
+            string query = @"
+                <QueryList>
+                    <Query Id=""0"" Path=""Security"">
+                        <Select Path=""Security"">
+                            *[System[EventID=4624]]
+                        </Select>
+                    </Query>
+                </QueryList>";
+
+            EventLogQuery eventQuery =
+                new EventLogQuery(
+                    "Security",
+                    PathType.LogName,
+                    query
+                )
+                {
+                    ReverseDirection = true
+                };
+
+            using EventLogReader reader =
+                new EventLogReader(eventQuery);
+
+            EventRecord? record;
+
+            int checkedEvents = 0;
+
+            while (
+                (record = reader.ReadEvent()) != null &&
+                checkedEvents < 100)
             {
-                string exePath =
-                    Process.GetCurrentProcess()
-                        .MainModule!
-                        .FileName!;
+                try
+                {
+                    checkedEvents++;
 
-                using RegistryKey? key =
-                    Registry.CurrentUser.OpenSubKey(
-                        RunKey,
-                        writable: true
-                    );
+                    if (!record.TimeCreated.HasValue)
+                        continue;
 
-                if (key == null)
-                    return;
+                    string xml =
+                        record.ToXml();
 
-                key.SetValue(
-                    AppName,
-                    $"\"{exePath}\""
-                );
+                    AuthenticationEvent eventInfo =
+                        ParseEvent(xml);
+
+                    if (!string.Equals(
+                        eventInfo.TargetUserSid,
+                        sid,
+                        StringComparison.OrdinalIgnoreCase))
+                    {
+                        continue;
+                    }
+
+                    LoginType? type =
+                        ConvertLogonType(
+                            eventInfo.LogonType
+                        );
+
+                    if (!type.HasValue)
+                        continue;
+
+                    return new LoginInfo
+                    {
+                        Time =
+                            record.TimeCreated.Value,
+
+                        Type =
+                            type.Value,
+
+                        LogonId =
+                            eventInfo.TargetLogonId,
+
+                        WindowsSessionId =
+                            GetCurrentSessionId()
+                    };
+                }
+                finally
+                {
+                    record.Dispose();
+                }
             }
-            catch
+
+            return null;
+        }
+
+        private LoginType? ConvertLogonType(
+            int logonType)
+        {
+            switch (logonType)
             {
-                // Ничего не делаем.
+                /*
+                 * На твоём ПК локальный вход
+                 * фиксируется как CachedInteractive.
+                 */
+                case 11:
+                    return LoginType.Local;
+
+                /*
+                 * Обычный Interactive.
+                 */
+                case 2:
+                    return LoginType.Local;
+
+                /*
+                 * Remote Interactive = RDP.
+                 */
+                case 10:
+                    return LoginType.Rdp;
+
+                /*
+                 * Cached Remote Interactive.
+                 */
+                case 12:
+                    return LoginType.Rdp;
+
+                default:
+                    return null;
             }
         }
 
-        public void DisableStartup()
+        private int GetCurrentSessionId()
         {
             try
             {
-                using RegistryKey? key =
-                    Registry.CurrentUser.OpenSubKey(
-                        RunKey,
-                        writable: true
-                    );
-
-                key?.DeleteValue(
-                    AppName,
-                    throwOnMissingValue: false
-                );
+                return
+                    System.Diagnostics.Process
+                        .GetCurrentProcess()
+                        .SessionId;
             }
             catch
             {
+                return -1;
             }
         }
 
-        public bool IsStartupEnabled()
+        private AuthenticationEvent
+            ParseEvent(string xml)
         {
+            AuthenticationEvent result =
+                new AuthenticationEvent();
+
             try
             {
-                using RegistryKey? key =
-                    Registry.CurrentUser.OpenSubKey(
-                        RunKey,
-                        writable: false
-                    );
+                XDocument document =
+                    XDocument.Parse(xml);
 
-                object? value =
-                    key?.GetValue(AppName);
+                XNamespace ns =
+                    "http://schemas.microsoft.com/win/2004/08/events/event";
 
-                return value != null;
+                foreach (
+                    XElement data
+                    in document.Descendants(ns + "Data"))
+                {
+                    string? name =
+                        data.Attribute("Name")?.Value;
+
+                    string value =
+                        data.Value;
+
+                    switch (name)
+                    {
+                        case "TargetUserSid":
+
+                            result.TargetUserSid =
+                                value;
+
+                            break;
+
+                        case "TargetLogonId":
+
+                            result.TargetLogonId =
+                                value;
+
+                            break;
+
+                        case "LogonType":
+
+                            if (int.TryParse(
+                                value,
+                                out int type))
+                            {
+                                result.LogonType =
+                                    type;
+                            }
+
+                            break;
+                    }
+                }
             }
             catch
             {
-                return false;
             }
+
+            return result;
+        }
+
+        private class AuthenticationEvent
+        {
+            public string TargetUserSid { get; set; } = "";
+
+            public string TargetLogonId { get; set; } = "";
+
+            public int LogonType { get; set; } = -1;
         }
     }
 }
