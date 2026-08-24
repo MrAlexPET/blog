9e22bc2c0fe0faff4d140f827c564721ecf0bce6
MrAlexPET <87830706+MrAlexPET@users.noreply.github.com>
2026-08-24 11:50:42 +0300

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,10 +1,2 @@
-Get-WinEvent -LogName Application -MaxEvents 50 |
-    Where-Object {
-        $_.Message -like "*CreateProcessAsUser*" -or
-        $_.Message -like "*WTSQueryUserToken*" -or
-        $_.Message -like "*DuplicateTokenEx*" -or
-        $_.Message -like "*CreateEnvironmentBlock*" -or
-        $_.Message -like "*PROCESS CREATED*"
-    } |
-    Select-Object TimeCreated, Message |
-    Format-List
+Get-Process LastAuthentication.UI -ErrorAction SilentlyContinue |
+    Select-Object Id, SessionId, StartTime, Path
