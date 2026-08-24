7d03f3757fe574a21ea527fad4cc8f6118ec1b2f
MrAlexPET <87830706+MrAlexPET@users.noreply.github.com>
2026-08-24 11:39:56 +0300

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,16 +1,5 @@
-TimeCreated      : 24.08.2026 11:23:02
-Id               : 0
-LevelDisplayName : Ошибка
-Message          : Category: LastAuthentication.Service.AuthenticationService
-                   EventId: 0
-
-                   Failed to launch LastAuthentication.UI.
-
-                   Exception:
-                   System.EntryPointNotFoundException: Unable to find an entry point named 'WTSGetActiveConsoleSessionI
-                   d' in DLL 'wtsapi32.dll'.
-                      at LastAuthentication.Service.SessionProcessLauncher.WTSGetActiveConsoleSessionId()
-                      at LastAuthentication.Service.SessionProcessLauncher.LaunchForActiveUser(String executablePath) i
-                   n D:\Login_program\LastAuthentication\LastAuthentication.Service\SessionProcessLauncher.cs:line 123
-                      at LastAuthentication.Service.AuthenticationService.LaunchUserInterface() in D:\Login_program\Las
-                   tAuthentication\LastAuthentication.Service\AuthenticationService.cs:line 190
+Get-CimInstance Win32_Process |
+    Where-Object {
+        $_.Name -eq "LastAuthentication.UI.exe"
+    } |
+    Select-Object ProcessId, SessionId, ExecutablePath, CommandLine
