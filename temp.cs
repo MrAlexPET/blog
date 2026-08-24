42c8a992c3bc9b11cbcdbe21b35df6686b8d74f2
MrAlexPET <87830706+MrAlexPET@users.noreply.github.com>
2026-08-24 11:38:39 +0300

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,7 +1,16 @@
-Get-WinEvent -LogName Application -MaxEvents 50 |
-    Where-Object {
-        $_.ProviderName -eq "LastAuthentication.Service" -or
-        $_.Message -like "*LastAuthentication.Service*"
-    } |
-    Select-Object TimeCreated, Id, LevelDisplayName, Message |
-    Format-List
+TimeCreated      : 24.08.2026 11:23:02
+Id               : 0
+LevelDisplayName : Ошибка
+Message          : Category: LastAuthentication.Service.AuthenticationService
+                   EventId: 0
+
+                   Failed to launch LastAuthentication.UI.
+
+                   Exception:
+                   System.EntryPointNotFoundException: Unable to find an entry point named 'WTSGetActiveConsoleSessionI
+                   d' in DLL 'wtsapi32.dll'.
+                      at LastAuthentication.Service.SessionProcessLauncher.WTSGetActiveConsoleSessionId()
+                      at LastAuthentication.Service.SessionProcessLauncher.LaunchForActiveUser(String executablePath) i
+                   n D:\Login_program\LastAuthentication\LastAuthentication.Service\SessionProcessLauncher.cs:line 123
+                      at LastAuthentication.Service.AuthenticationService.LaunchUserInterface() in D:\Login_program\Las
+                   tAuthentication\LastAuthentication.Service\AuthenticationService.cs:line 190
