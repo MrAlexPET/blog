dca5f7e5e85a8b76de3a56181e35dea28d0988db
MrAlexPET <87830706+MrAlexPET@users.noreply.github.com>
2026-08-24 12:11:32 +0300

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,11 +1,6 @@
-info: LastAuthentication.Service.AuthenticationService[0]
-      Attempting to launch UI: D:\Login_program\LastAuthentication\LastAuthentication.Service\bin\Debug\net8.0-windows\LastAuthentication.UI.exe
-[SessionProcessLauncher] Searching active sessions...
-[SessionProcessLauncher] Session=0, State=4
-[SessionProcessLauncher] Session=2, State=0
-[SessionProcessLauncher] Trying session 2
-[SessionProcessLauncher] WTSQueryUserToken(2)
-[SessionProcessLauncher] Session 2 failed: WTSQueryUserToken failed for session 2.
-[SessionProcessLauncher] Session=65536, State=6
-info: LastAuthentication.Service.AuthenticationService[0]
-      UI launch result: False
+Get-WinEvent -LogName Application -MaxEvents 30 |
+    Where-Object {
+        $_.ProviderName -like "*LastAuthentication*"
+    } |
+    Select-Object TimeCreated, ProviderName, Message |
+    Format-List
